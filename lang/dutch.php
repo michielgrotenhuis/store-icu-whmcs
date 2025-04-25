9b886285f62285b6040b75ee4eaf3b6b04dc0cea
michielgrotenhuis <michiel@cmy.nl>
2025-04-25 23:04:51 +0200

    Create dutch.php

---
+++ lang/dutch.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * WHMCS Store.icu Provisioning Module
+ * Dutch Language File
+ */
+
+if (!defined("WHMCS")) {
+    die("This file cannot be accessed directly");
+}
+
+$_LANG['store_icu'] = array(
+    'module_name' => 'Store.icu Ecommerce Sitebuilder',
+    'module_description' => 'Integratie met Store.icu ecommerce sitebuilder voor webhosting bedrijven.',
+    
+    // Module settings
+    'distributor_email' => 'Distributeur E-mail ID',
+    'distributor_email_desc' => 'Distributeur of reseller of subreseller e-mail ID',
+    'distributor_password' => 'Distributeur Wachtwoord',
+    'distributor_password_desc' => 'Distributeur of reseller of subreseller e-mail ID wachtwoord',
+    'default_currency' => 'Standaard Winkel Valuta',
+    'default_currency_desc' => 'Standaard valuta voor nieuwe winkels (bijv. EUR)',
+    'default_timezone' => 'Standaard Winkel Tijdzone',
+    'default_timezone_desc' => 'Standaard tijdzone voor nieuwe winkels (bijv. Europe/Amsterdam)',
+    'default_country' => 'Standaard Winkel Land',
+    'default_country_desc' => 'Standaard landcode voor nieuwe winkels (ISO 2-letter code)',
+    'default_language' => 'Standaard Winkel Taal',
+    'default_language_desc' => 'Standaard taal voor nieuwe winkels (bijv. nl-NL)',
+    'package_type' => 'Pakkettype',
+    'package_type_desc' => 'Pakkettype voor nieuwe winkels',
+    
+    // Client area
+    'store_active' => 'Uw winkel is actief en operationeel.',
+    'store_suspended' => 'Uw winkel is momenteel opgeschort. Neem contact op met de klantenservice.',
+    'store_terminated' => 'Uw winkel is beëindigd.',
+    'store_info' => 'Winkelinformatie',
+    'shop_handle' => 'Winkel Naam',
+    'status' => 'Status',
+    'created' => 'Aangemaakt',
+    'quick_links' => 'Snelkoppelingen',
+    'login_to_store' => 'Inloggen bij Winkel',
+    'store_statistics' => 'Winkelstatistieken',
+    'statistics_future' => 'Winkelstatistieken worden hier weergegeven in toekomstige updates.',
+    'error_occurred' => 'Er is een fout opgetreden',
+    'contact_support' => 'Contact Klantenservice',
+    
+    // Admin area
+    'no_store_found' => 'Geen winkelinformatie gevonden voor deze dienst',
+    'api_auth_error' => 'API Authenticatiefout',
+    'api_error' => 'API Fout',
+    'stores' => 'Winkels',
+    'no_stores_found' => 'Geen winkels gevonden voor deze gebruiker',
+    
+    // Sidebar
+    'store_icu_details' => 'Store.icu Details',
+    
+    // Homepage panel
+    'my_online_stores' => 'Mijn Online Winkels',
+    'manage' => 'Beheren',
+    
+    // Button texts
+    'sso_button' => 'Inloggen bij Winkel',
+);
