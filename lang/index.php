d5ec12e449c07b3b795265f5141c5f639e006697
michielgrotenhuis <michiel@cmy.nl>
2025-04-25 23:05:07 +0200

    Create index.php

---
+++ lang/index.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * WHMCS Store.icu Provisioning Module
+ * Language Loader
+ * 
+ * This file prevents direct access to the language files
+ */
+
+header("Location: ../");
+die("This file cannot be accessed directly");
