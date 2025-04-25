af37295009aab4a5f76fb7c1a9fc549c18d2a6fb
michielgrotenhuis <michiel@cmy.nl>
2025-04-25 23:04:31 +0200

    Create english.php

---
+++ lang/english.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * WHMCS Store.icu Provisioning Module
+ * English Language File
+ */
+
+if (!defined("WHMCS")) {
+    die("This file cannot be accessed directly");
+}
+
+$_LANG['store_icu'] = array(
+    'module_name' => 'Store.icu Ecommerce Sitebuilder',
+    'module_description' => 'Integration with Store.icu ecommerce sitebuilder for web hosting companies.',
+    
+    // Module settings
+    'distributor_email' => 'Distributor Email ID',
+    'distributor_email_desc' => 'Distributor or reseller or subreseller email ID',
+    'distributor_password' => 'Distributor Password',
+    'distributor_password_desc' => 'Distributor or reseller or subreseller email ID password',
+    'default_currency' => 'Default Store Currency',
+    'default_currency_desc' => 'Default currency for new stores (e.g., USD)',
+    'default_timezone' => 'Default Store Timezone',
+    'default_timezone_desc' => 'Default timezone for new stores (e.g., America/New_York)',
+    'default_country' => 'Default Store Country',
+    'default_country_desc' => 'Default country code for new stores (ISO 2-letter code)',
+    'default_language' => 'Default Store Language',
+    'default_language_desc' => 'Default language for new stores (e.g., en-US)',
+    'package_type' => 'Package Type',
+    'package_type_desc' => 'Package type for new stores',
+    
+    // Client area
+    'store_active' => 'Your store is active and running.',
+    'store_suspended' => 'Your store is currently suspended. Please contact support.',
+    'store_terminated' => 'Your store has been terminated.',
+    'store_info' => 'Store Information',
+    'shop_handle' => 'Shop Handle',
+    'status' => 'Status',
+    'created' => 'Created',
+    'quick_links' => 'Quick Links',
+    'login_to_store' => 'Login to Store',
+    'store_statistics' => 'Store Statistics',
+    'statistics_future' => 'Store statistics will be displayed here in future updates.',
+    'error_occurred' => 'An error occurred',
+    'contact_support' => 'Contact Support',
+    
+    // Admin area
+    'no_store_found' => 'No store information found for this service',
+    'api_auth_error' => 'API Authentication Error',
+    'api_error' => 'API Error',
+    'stores' => 'Stores',
+    'no_stores_found' => 'No stores found for this user',
+    
+    // Sidebar
+    'store_icu_details' => 'Store.icu Details',
+    
+    // Homepage panel
+    'my_online_stores' => 'My Online Stores',
+    'manage' => 'Manage',
+    
+    // Button texts
+    'sso_button' => 'Login to Store',
+);
